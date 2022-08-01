dabc9830127b4083ef491e0db5bd6bb0e2b49a53
Bekzod <bekzod.k2407@gmail.com>
2022-08-01 11:03:13 +0500

    added tabbar

---
+++ ScheduleApp/Controllers/ContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactsViewController.swift
+//  ScheduleApp
+//
+//  Created by Bekzod Qahhorov on 01/08/22.
+//
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Contacts"
+        
+    }
+}
+
